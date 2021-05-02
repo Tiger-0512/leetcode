b0ae82290d62af35d855cac866a778041fde3ac8
Tiger-0512 <63806957+Tiger-0512@users.noreply.github.com>
2021-05-02 11:44:11 +0900

    Add new solutions

---
+++ go/50_powx-n.go
@@ -0,0 +1,11 @@
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	} else if n < 0 {
+		return myPow(1/x, -n)
+	} else if n%2 == 0 {
+		return myPow(x*x, n/2)
+	} else {
+		return x * myPow(x*x, n/2)
+	}
+}
