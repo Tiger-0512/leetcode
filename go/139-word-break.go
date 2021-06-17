acfc5ad474c862b0c9e07546d79dfd3bac9338b1
Tiger-0512 <63806957+Tiger-0512@users.noreply.github.com>
2021-06-18 00:51:30 +0900

    Add new solutions

---
+++ go/139-word-break.go
@@ -0,0 +1,20 @@
+func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+
+	for i := range s {
+		if !dp[i] {
+			continue
+		}
+
+		for _, word := range wordDict {
+			if i+len(word) > len(s) {
+				continue
+			}
+			if word == s[i:i+len(word)] {
+				dp[i+len(word)] = true
+			}
+		}
+	}
+	return dp[len(s)]
+}
